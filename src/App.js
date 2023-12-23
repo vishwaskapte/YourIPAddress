21bf416bcc0d49ec892abe1e2dbad0bc0524bb47
vishwaskapte <45562090+vishwaskapte@users.noreply.github.com>
2023-12-24 00:04:48 +0530

    Code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+//1 Import Area
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
 
+//2. Function Defination Area
 function App() {
+  //1. Hooks Area
+  //creating IP state
+    const [ip,setIP] = useState('');
+
+
+  //2. Function Area
+  //creating function to load ip address from the API
+    const getData = async()=>{
+      const res = await axios.get('https://geolocation-db.com/json/')
+      console.log(res.data);
+      setIP(res.data.IPv4)
+    }
+
+    useEffect(()=>{
+        //passing getData method to the lifecycle method
+        getData()
+    },[])
+
+  //3. Return Area
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+            <h2>Your IP Address is</h2>
+            <h4>{ip}</h4>
     </div>
   );
 }
 
+//3. Export Area
 export default App;
